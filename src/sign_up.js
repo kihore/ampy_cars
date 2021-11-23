216ee44977a7030a825642d994feabed5f2e97dc
kihore <kannankishore20@gmail.com>
2021-11-23 19:20:46 +0530

    master

---
+++ src/sign_up.js
@@ -0,0 +1,62 @@
+import React,{useState}from 'react'
+
+function Signup() {
+    const initialvalues = {username:"",email:"",password:"",confirm_password:""};
+    const [Data,setData] = useState(initialvalues);
+    const [error,seterror] = useState({});
+    const [issubmit,setissubmit] = useState(false);
+
+    const handlechange = (e) =>{
+        const {name,value} = e.target;
+        setData ({...setData, [name]: value});
+    };
+    const handlesubmit = (e) =>{
+        e.preventDefault();
+        setissubmit(true);
+        seterror(validate(Data));
+    };
+    const validate = (value) =>{
+        const errors = {};
+        if(!value.username){
+            errors.username= 'user name is required';
+        }
+        if(!value.email){
+            errors.email= 'email is required';
+        }
+        if(!value.password){
+            errors.password= 'password is required';
+        }
+        if(!value.confirm_password){
+            errors.confirm_password= 're-enter the password';
+        }
+    }
+    return (
+        <div className='container'>
+            <form onSubmit= {handlesubmit}>
+            <h2>Sign Up</h2>
+            <div className='ui divider'></div>
+            <div className= 'ui form'>
+                <div className = 'field'>
+                    <label>Username</label>
+                    <input type= 'text' name= 'Name' placeholder= 'Enter the user name' value= {Data.username} onChange= {handlechange}/>
+                </div>
+                <div className = 'field'>
+                    <label>Email</label>
+                    <input type= 'text' name= 'Email' placeholder= 'Enter the email-id' value= {Data.email} onChange= {handlechange}/>
+                </div>
+                <div className = 'field'>
+                    <label>password</label>
+                    <input type= 'text' name= 'Password' placeholder= 'Enter the password' value= {Data.password} onChange= {handlechange}/>
+                </div>
+                <div className = 'field'>
+                    <label>confirm password</label>
+                    <input type= 'text' name= 'Confirm password' placeholder= 'confirm password' value= {Data.confirm_password} onChange= {handlechange}/>
+                </div>
+                <button className= 'usebutton'>Sign Up</button>
+            </div>
+            </form>
+        </div>
+    )
+}
+
+export default Signup
