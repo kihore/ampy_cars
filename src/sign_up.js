afc84e55cf9e034b405617566480e31cd39f9a8e
kihore <kannankishore20@gmail.com>
2021-11-25 22:38:33 +0530

    second changes

--- src/sign_up.js
+++ src/sign_up.js
@@ -1,77 +1,45 @@
-import React,{useState, useEffect}from 'react';
-import axios from 'axios';
+import React,{useState}from 'react';
 
 function Signup() {
     const initialvalues = {username:"",email:"",password:"",confirm_password:""};
     const [Data,setData] = useState(initialvalues);
-    const [error,seterror] = useState({});
-    const [issubmit,setissubmit] = useState(false);
 
-    const handlechange = (e) =>{
-        const {name,value} = e.target;
+    const handlechange = (event) =>{
+        const {name,value} = event.target;
         setData ({...setData, [name]: value});
     };
-    const handlesubmit = (e) =>{
-        axios.post("http://localhost:9000/get", {username:document.getElementsId("Name").value,email:document.getElementsId("Email").value,
-        password:document.getElementsId("Password").value,
-        confirm_password:document.getElementsId("Confirm password").value} )
-        .then(resp=>{console.log(resp.Data)
-        console.log()})
-        .catch(function(errors){
-            console.log(errors)
-        })
-        e.preventDefault();
-        console.log(e);
-        setissubmit(true);
-        seterror(validate(Data));
-    };
-    useEffect(() =>{
-        if(Object.keys(error).length === 0 && issubmit){
-            console.log(Data);
-        }
-    },[error]);
-    const validate = (value) =>{
-        const errors = {};
-        if(!value.username){
-            errors.username= 'user name is required';
-        }
-        if(!value.email){
-            errors.email= 'email is required';
-        }
-        if(!value.password){
-            errors.password= 'password is required';
-        }
-        if(!value.confirm_password){
-            errors.confirm_password= 're-enter the password';
-        }
-        return errors;
-    };
+
     return (
         <div className='container'>
-            <form onSubmit= {handlesubmit} id="form">
+            <form>
             <h2>Sign Up</h2>
             <div className='ui divider'></div>
             <div className= 'ui form'>
                 <div className = 'field'>
                     <label>Username</label>
-                    <input type= 'text' name= 'Name' placeholder= 'Enter the user name' value= {Data.username} onChange= {handlechange}/>
+                    <input type= 'text' name= 'Name' placeholder= 'Enter the user name'
+                     value= {Data.username} onChange= {handlechange}  
+                     required 
+                     pattern= '^[a-zA-Z0-9_]{5,}[a-zA-Z]+[0-9]*$'/>
                 </div>
-                <p>{error.username}</p>
+                
                 <div className = 'field'>
                     <label>Email</label>
-                    <input type= 'text' name= 'Email' placeholder= 'Enter the email-id' value= {Data.email} onChange= {handlechange}/>
+                    <input type= 'text' name= 'Email' placeholder= 'Enter the email-id' value= {Data.email} onChange= {handlechange} required/>
                 </div>
-                <p>{error.email}</p>
+               
                 <div className = 'field'>
                     <label>password</label>
-                    <input type= 'password' name= 'Password' placeholder= 'Enter the password' value= {Data.password} onChange= {handlechange}/>
+                    <input type= 'password' name= 'Password' placeholder= 'Enter the password' value= {Data.password} 
+                    onChange= {handlechange}
+                     required
+                     pattern= '^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$' />
                 </div>
-                <p>{error.password}</p>
+                
                 <div className = 'field'>
                     <label>confirm password</label>
-                    <input type= 'password' name= 'Confirm password' placeholder= 'confirm password' value= {Data.confirm_password} onChange= {handlechange}/>
+                    <input type= 'password' name= 'Confirm password' placeholder= 'confirm password' value= {Data.confirm_password} onChange= {handlechange} required  pattern= 'value.Password'/>
                 </div>
-                <p>{error.confirm_password}</p>
                 <button className= 'usebutton'>Sign Up</button>
             </div>
             </form>
